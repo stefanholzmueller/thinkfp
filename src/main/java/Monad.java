3a07624131e34e0759408e6b6df2d6ce559c6d02
Stefan Holzmüller <stefanholzmueller@github>
2014-02-09 05:34:36 +0100

    monad interface experiment

--- src/main/java/Monad.java
+++ src/main/java/Monad.java
@@ -1,5 +1,10 @@
 import java.util.function.Function;
 
 public interface Monad<T> {
-	<R> Monad<R> bind(Function<T, Monad<R>> f);
+	<U extends T> Monad<U> unit(U value);
+	<R extends T> Monad<R> bind(Function<T, Monad<R>> f);
+
+	default <R extends T> Monad<T> map(Function<T, R> f) {
+		return bind((Function<T, Monad<T>>) x ->  unit(f.apply(x)));
+	}
 }
