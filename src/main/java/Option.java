d648dc3accb1088b971c2f7f86201e69d7e2ed06
Stefan Holzmüller <stefanholzmueller@github>
2014-02-09 05:05:11 +0100

    bind works

--- src/main/java/Option.java
+++ src/main/java/Option.java
@@ -3,21 +3,25 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import java.util.ArrayList;
 import java.util.function.Function;
 
-public abstract class Option<T> implements Monad<T> {
+public abstract class Option<T> {
 
+	public abstract <R> Option<R> bind(Function<T, Option<R>> f);
+	public abstract boolean isEmpty();
 	public abstract T getOrElse(T elseValue);
-
-	private static final Option NONE = new None();
 
 	private Option() {}
 
+	private static final Option NONE = new None();
 	private static final class None<T> extends Option<T> {
 
 		@Override
-		public <R> Option<R> bind(Function<T, Monad<R>> f) {
+		public <R> Option<R> bind(Function<T, Option<R>> f) {
 			return NONE;
 		}
-
+		@Override
+		public boolean isEmpty() {
+			return true;
+		}
 		@Override
 		public T getOrElse(T elseValue) {
 			return elseValue;
@@ -25,17 +29,20 @@ public abstract class Option<T> implements Monad<T> {
 	}
 
 	private static final class Some<T> extends Option<T> {
-		private final T value;
 
+		private final T value;
 		public Some(T value) {
 			this.value = value;
 		}
 
 		@Override
-		public <R> Option<R> bind(Function<T, Monad<R>> f) {
-			throw new NotImplementedException();
+		public <R> Option<R> bind(Function<T, Option<R>> f) {
+			return f.apply(this.value);
 		}
-
+		@Override
+		public boolean isEmpty() {
+			return false;
+		}
 		@Override
 		public T getOrElse(T elseValue) {
 			return this.value;
--- src/test/java/OptionTest.java
+++ src/test/java/OptionTest.java
@@ -1,11 +1,25 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.function.Function;
+
 public class OptionTest {
 
+	public static final Option<Object> NONE = Option.unit(null);
+
 	@Test
-	public void getOrElse_null_elseValue() {
-		Assert.assertEquals("else", Option.unit(null).getOrElse("else"));
+	public void isEmpty_none_true() {
+		Assert.assertTrue(NONE.isEmpty());
+	}
+
+	@Test
+	public void isEmpty_some_false() {
+		Assert.assertFalse(Option.unit("some").isEmpty());
+	}
+
+	@Test
+	public void getOrElse_none_elseValue() {
+		Assert.assertEquals("else", NONE.getOrElse("else"));
 	}
 
 	@Test
@@ -13,5 +27,37 @@ public class OptionTest {
 		Assert.assertEquals("some", Option.unit("some").getOrElse("else"));
 	}
 
+	@Test
+	public void bind_noneAndNone_none() {
+		Option<String> noneStr = Option.unit((String) null);
+		Function<String, Option<Integer>> f = (String s) -> Option.unit(null);
+		Option<Integer> optInt = noneStr.bind(f);
+		Assert.assertTrue(optInt.isEmpty());
+	}
+
+	@Test
+	public void bind_noneAndSome_none() {
+		Option<String> noneStr = Option.unit((String) null);
+		Function<String, Option<Integer>> f = (String s) -> Option.unit(s.length());
+		Option<Integer> optInt = noneStr.bind(f);
+		Assert.assertTrue(optInt.isEmpty());
+	}
+
+	@Test
+	public void bind_someAndNone_none() {
+		Option<String> noneStr = Option.unit("some");
+		Function<String, Option<Integer>> f = (String s) -> Option.unit(null);
+		Option<Integer> optInt = noneStr.bind(f);
+		Assert.assertTrue(optInt.isEmpty());
+	}
+
+	@Test
+	public void bind_someAndSome_Some() {
+		Option<String> noneStr = Option.unit("some");
+		Function<String, Option<Integer>> f = (String s) -> Option.unit(s.length());
+		Option<Integer> optInt = noneStr.bind(f);
+		Assert.assertFalse(optInt.isEmpty());
+		Assert.assertEquals(4, (int) optInt.getOrElse(0));
+	}
 
 }
