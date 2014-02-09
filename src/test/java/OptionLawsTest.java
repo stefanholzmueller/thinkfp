e1e5cadcd7b72f11e5abbb875796bdbb23f47ba6
Stefan Holzmüller <stefanholzmueller@github>
2014-02-09 10:46:03 +0100

    shortened laws test

--- src/test/java/OptionLawsTest.java
+++ src/test/java/OptionLawsTest.java
@@ -7,9 +7,11 @@ import static junit.framework.Assert.assertEquals;
 
 public class OptionLawsTest {
 
+	private String x = "test";
+	private Option<String> m = Option.unit(x);
+
 	@Test
 	public void associativity() {
-		Option<String> m = Option.unit("s");
 		Function<String, Option<String>> f = s -> Option.unit(s.toUpperCase());
 		Function<String, Option<String>> g = s -> Option.unit(s + " " + s);
 		assertEquals(m.bind(f).bind(g), m.bind((String x) -> f.apply(x).bind(g)));
@@ -17,14 +19,12 @@ public class OptionLawsTest {
 
 	@Test
 	public void leftUnit() {
-		String x = "s";
 		Function<String, Option<Integer>> f = s -> Option.unit(s.length());
 		assertEquals(f.apply(x), Option.unit(x).bind(f));
 	}
 
 	@Test
 	public void rightUnit() {
-		Option<String> m = Option.unit("s");
 		assertEquals(m, m.bind(Option::unit));
 	}
 
