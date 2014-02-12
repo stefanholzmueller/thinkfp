8e6fc76a906a90b08e753d984ebe1bf3fbedf876
Stefan Holzmüller <stefanholzmueller@github>
2014-02-12 21:14:08 +0100

    tests for None

--- src/test/java/OptionLawsTest.java
+++ src/test/java/OptionLawsTest.java
@@ -8,6 +8,7 @@ public class OptionLawsTest {
 
 	private String s = " test ";
 	private Option<String> m = Option.unit(s);
+	private Option<String> n = Option.unit(null);
 
 	@Test
 	public void associativity() {
@@ -17,14 +18,32 @@ public class OptionLawsTest {
 	}
 
 	@Test
+	public void associativityNone() {
+		Function<String, Option<String>> f = s -> Option.unit(s.trim());
+		Function<String, Option<Integer>> g = s -> Option.unit(s.length());
+		assertEquals(n.bind(f).bind(g), n.bind(x -> f.apply(x).bind(g)));
+	}
+
+	@Test
 	public void leftIdentity() {
 		Function<String, Option<Integer>> f = s -> Option.unit(s.length());
 		assertEquals(f.apply(s), Option.unit(s).bind(f));
 	}
 
 	@Test
+	public void leftIdentityNone() {
+		Function<String, Option<Integer>> f = s -> s == null ? Option.unit(null) : Option.unit(s.length());
+		assertEquals(f.apply(null), Option.<String>unit(null).bind(f));
+	}
+
+	@Test
 	public void rightIdentity() {
 		assertEquals(m, m.bind(Option::unit));
+	}
+
+	@Test
+	public void rightIdentityNone() {
+		assertEquals(n, n.bind(Option::unit));
 	}
 
 }
